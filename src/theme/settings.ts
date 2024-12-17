47137198e103bbd1d83d1afb32f6cb238a980ce1
xiatian <939648675@qq.com>
2024-12-17 10:57:54 +0800

    修改配置

--- src/theme/settings.ts
+++ src/theme/settings.ts
@@ -51,7 +51,7 @@ export const themeSettings: App.Theme.ThemeSetting = {
   },
   watermark: {
     visible: false,
-    text: 'SoybeanAdmin'
+    text: 'JinLinkAdmin'
   },
   tokens: {
     light: {
@@ -85,6 +85,6 @@ export const themeSettings: App.Theme.ThemeSetting = {
 export const overrideThemeSettings: Partial<App.Theme.ThemeSetting> = {
   watermark: {
     visible: false,
-    text: 'SoybeanAdmin'
+    text: 'JinLinkAdmin'
   }
 };
